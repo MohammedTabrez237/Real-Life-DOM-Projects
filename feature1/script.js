025ec24a686459f3d5d133cf5c3d786b576f899e
MohammedTabrez237 <mdtabrezk281@gmail.com>
2025-10-06 10:30:35 +0530

    added new feature2

--- feature1/script.js
+++ feature1/script.js
@@ -1,1 +1,21 @@
-document.querySelector
+var istatus = document.querySelector("h5")
+
+var btn = document.querySelector('#add');
+var check = 0
+
+btn.addEventListener("click",function(){
+    if (check == 0){
+        istatus.innerHTML ="Friends";
+        istatus.style.color = "rgb(93, 103, 215)";
+        btn.innerHTML = "Remove Friend";
+        check = 1
+    }else{
+        istatus.innerHTML ="Strangers";
+        istatus.style.color = "rgba(235, 24, 24, 1)";
+        btn.innerHTML = "Add Friend";
+        check = 0;
+
+    }
+    
+})
+
